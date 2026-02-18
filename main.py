ab96574bf424e7480f6ae302fd88683d7b9c85e3
Sakthi <sakthieditz99@gmail.com>
2026-02-18 23:43:38 +0530

    first commit

--- main.py
+++ main.py
@@ -17,14 +17,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # Vite default
-        "http://localhost:3000",  # CRA default (optional)
+        "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 def get_db():
     db = SessionLocal()
